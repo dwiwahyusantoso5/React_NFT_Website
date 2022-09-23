c68b35bc0995a62a3490db5b81ddf6c1ff346034
intking <dwi.wahyu.toso@gmail.com>
2022-09-23 19:48:29 +0700

    add live-auction

--- src/components/ui/Live-auction/LiveAauction.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const LiveAauction = () => {
-  return <div>LiveAauction</div>;
-};
-
-export default LiveAauction;
-
-//min 1.8.26
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import { Container } from "react-dom";
 import HeroSection from "../components/ui/HeroSection";
+import LiveAuction from "../components/ui/Live-auction/LiveAuction";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <LiveAuction />
     </>
   );
 };
